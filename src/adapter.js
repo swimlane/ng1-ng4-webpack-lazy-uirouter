d1ea46d826c770380836644c9542b1b8dfd2aab0
Austin McDaniel <amcdaniel2@gmail.com>
2016-08-12 16:36:54 -0400

    better adapter

--- src/adapter.js
+++ src/adapter.js
@@ -3,15 +3,13 @@ import { uiRouterNgUpgrade } from 'ui-router-ng1-to-ng2';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-const Ng2AppModule =
-  NgModule({
-    declarations: [],
-    imports: [BrowserModule],
-    providers: []
-  }).Class({
-    constructor: function Ng2AppModule() {},
-    ngDoBootstrap: function() {}
-  });
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule]
+})
+class Ng2AppModule {
+  ngDoBootstrap(){ }
+}
 
 export const adapter = new UpgradeAdapter(Ng2AppModule);
 uiRouterNgUpgrade.setUpgradeAdapter(adapter);
