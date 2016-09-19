e4652562ae7f1e38a1645a713c30cc77a220a906
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-19 15:30:37 -0400

    (bug): fix adapter changes

--- src/adapter.js
+++ src/adapter.js
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { UpgradeAdapter } from '@angular/upgrade';
 
 import { UIRouterModule } from 'ui-router-ng2';
-import { uiRouterNgUpgrade } from 'ui-router-ng1-to-ng2';
+import { uiRouterNgUpgrade, Ng1ToNg2Module } from 'ui-router-ng1-to-ng2';
 
-
+// Use @UIRouterModule instead of @NgModule to allow use of the UIRouter directives
+// and add the UIRouter providers to the root ng2 injector
 @UIRouterModule({
-  imports: [BrowserModule]
+  imports: [BrowserModule, Ng1ToNg2Module]
 })
-class Ng2AppModule { }
+class AppModule { }
 
-export const adapter = new UpgradeAdapter(Ng2AppModule);
+// Create ngUpgrade Adapter
+export const adapter = new UpgradeAdapter(AppModule);
+
+// Bootstrap ui-router
 uiRouterNgUpgrade.setUpgradeAdapter(adapter);
+
+// Register ng1 servers as ng2 providers
+upgradeAdapter.upgradeNg1Provider('$state');
