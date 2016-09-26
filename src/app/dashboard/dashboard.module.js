231b7ec982d036150a73a09fe5674fe0e0dce9d7
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-26 17:01:10 -0400

    new edge case

--- src/app/dashboard/dashboard.module.js
+++ src/app/dashboard/dashboard.module.js
@@ -1,11 +1,5 @@
 import angular from 'angular';
-import { adapter } from '../../adapter.js';
-
-import { DashboardComponent } from './dashboard.component.js';
 import { configState } from './dashboard.state.js';
-
-// const dashboardComponent =
-//  adapter.downgradeNg2Component(DashboardComponent));
 
 export default angular
   .module('dashboard', [])
--- src/app/dashboard/dashboard.state.js
+++ src/app/dashboard/dashboard.state.js
@@ -1,11 +1,15 @@
+import { adapter } from '../../adapter.js';
 import { DashboardComponent } from './dashboard.component.js';
+
+const dashboardComponent = adapter.downgradeNg2Component(DashboardComponent);
 
 export function configState($stateProvider) {
   $stateProvider.state('dashboard', {
     url: '/dashboard',
     views: {
       $default: {
-        component: DashboardComponent,
+        // component: DashboardComponent,
+        controller: dashboardComponent,
         resolve: {
           // wanna keep this for something
           // I might wanna DI in that isn't
