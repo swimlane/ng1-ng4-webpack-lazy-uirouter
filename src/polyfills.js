06345bf3aa34b72636cdc6b303f6c5acf1ab8bc8
Austin McDaniel <amcdaniel2@gmail.com>
2016-09-27 11:06:58 -0400

    better stack tracing

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,4 +1,8 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'babel-polyfill';
+
 import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+
+Error.stackTraceLimit = Infinity;
