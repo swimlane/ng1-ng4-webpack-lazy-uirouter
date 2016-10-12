c32af7476ec019f1a5f69a3a7a48a4da6fe93c4f
Chris Thielen <github@sandgnat.com>
2016-10-12 18:19:14 -0500

    fix(lazyLoad): Wait to lazy load until a future state is activated

--- src/app/app.state.js
+++ src/app/app.state.js
@@ -4,11 +4,11 @@ export const MAIN_STATES = [
   {
     name: 'login',
     url: '/login',
-    lazyLoad: loadNg1Module(System.import('app/login/login.module.js'))
+    lazyLoad: loadNg1Module(() => System.import('app/login/login.module.js'))
   },
   {
     name: 'admin',
     url: '/admin',
-    lazyLoad: loadNg2Default(System.import('app/admin/admin.module.js'))
+    lazyLoad: loadNg2Default(() => System.import('app/admin/admin.module.js'))
   }
 ];
--- src/utils/loadNg1Module.js
+++ src/utils/loadNg1Module.js
@@ -1,9 +1,9 @@
-export function loadNg1Module(module) {
+export function loadNg1Module(getModule) {
   return (transition) => {
     let $ocLazyLoad = transition.injector().get('$ocLazyLoad');
 
     return new Promise((success, fail) => {
-      module.then(newModule => {
+      getModule().then(newModule => {
         if(!newModule.name) newModule = newModule.default;
         $ocLazyLoad.load(newModule).then(success, fail);
       });
