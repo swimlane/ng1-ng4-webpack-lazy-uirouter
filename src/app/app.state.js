f88788bed8dee223d7bacacf827a0dea248ac133
Chris Thielen <github@sandgnat.com>
2017-03-24 12:19:20 -0500

    Add visualizer. Fix  is not defined. Update future states to new style

--- src/app/app.state.js
+++ src/app/app.state.js
@@ -2,12 +2,12 @@ import { loadNg1Module } from '../utils/loadNg1Module.js';
 
 export const MAIN_STATES = [
   {
-    name: 'login',
+    name: 'login.**',
     url: '/login',
     lazyLoad: loadNg1Module(() => System.import('app/login/login.module.js'))
   },
   {
-    name: 'admin',
+    name: 'admin.**',
     url: '/admin',
     loadChildren: () => System.import('app/admin/admin.module.js').then(m => m.default)
   }
